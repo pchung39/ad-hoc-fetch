346af853f5059f46ceea6d03b62440921436ba62
Paul Chung <paulchung39@gmail.com>
2017-07-12 12:18:20 -0700

    cleaned up .then functions

--- api/managed-records.js
+++ api/managed-records.js
@@ -16,12 +16,12 @@ var retrieve = (options = {}) => {
     .then(response => {
       let json = response.json();
       if (response.ok) {
-        return json
+        return json;
       } else {
         return json.then(Promise.reject.bind(Promise));
       }
     })
-    .then(response => returnRecords(response, options, queryString))
+    .then(response => returnRecords(response, options))
     .catch(e => console.log("error: ", e))
 
 
@@ -34,23 +34,23 @@ Additional validation is used in the cases where a page or colors are not provid
 var buildQueryString = (options, url) => {
 
   let colors = options["colors"] ? options["colors"] : ["red", "brown", "blue", "yellow", "green"];
-  let page = options["page"] ? (options["page"] * 10) - 10 : 0;
+  let offset = options["page"] ? (options["page"] * 10) - 10 : 0;
 
   let queryString = URI(url)
     .addSearch("limit", 10)
-    .addSearch("offset", page)
-    .addSearch("color[]", colors)
+    .addSearch("offset", offset)
+    .addSearch("color[]", colors);
 
-  return queryString
+  return queryString;
 };
 
 /*
-Once the "/records" api completes, the returned reponse is formatted as per the instructions.
+Once the "/records" fetch completes, the returned reponse is formatted as per the instructions.
 In order to provide a previous and next page, two additional fetches need to be made to check whether there
 is additional data immediately before and after the initial request. This process is encapsulated by the
-`getPageAssets` function called inside this function.
+`getPageAssets` function called inside the `returnRecords` function.
 */
-var returnRecords = (responseList, options, queryString) => {
+var returnRecords = (responseList, options) => {
 
   return getPageAssets(responseList, options)
   .then(response => formatRecords(response, responseList, options))
@@ -105,28 +105,25 @@ var formatRecords = (pageResults, responseList, options) => {
     closedPrimaryCount: closedCount
   };
 
-  return recordObject
+  return recordObject;
 
 
 }
 
 
 /*
-In order to calculate whether there is a previous and next page, a simple call is made for the 10 entries
-immediately before and after the initial fetch request. The results will always be 1 of 2 outcomes, either the
-fetch response will be an empty list or a populated list. With this information we can make an informed
-decision on what the previous and next page numbers will be.
+Executes fetches for previous and next pages and returns a page object.
 */
 var getPageAssets = (responseList, options) => {
 
+  let colors = options["colors"]
+  let currentPage = options["page"] ? options["page"]: 1;
   let previousPageCall;
   let nextPageCall;
   let pageObject = {
     previous: null,
     next: null
   };
-  let colors = options["colors"]
-  let currentPage = options["page"] ? options["page"]: 1;
 
   if (currentPage == 1) {
 
@@ -135,7 +132,7 @@ var getPageAssets = (responseList, options) => {
   } else {
 
     previousPageCall = fetchNextAndPreviousPages(path,{ page: currentPage - 1, colors: colors })
-      .then(response => { pageObject["previous"] = response.length > 0 ? currentPage - 1 : null })
+      .then(response => pageObject["previous"] = response.length > 0 ? currentPage - 1 : null )
       .catch(e => console.log("error: ", e))
   }
 
@@ -146,13 +143,12 @@ var getPageAssets = (responseList, options) => {
   } else {
 
     nextPageCall = fetchNextAndPreviousPages(path, { page: currentPage + 1, colors: colors })
-      .then(response => { pageObject["next"] = response.length > 0 ? currentPage + 1 : null })
+      .then(response => pageObject["next"] = response.length > 0 ? currentPage + 1 : null )
       .catch(e => console.log("error: ", e))
   }
 
-
   return Promise.all([previousPageCall, nextPageCall])
-    .then(response => {return pageObject})
+    .then(response => pageObject)
     .catch(e => console.log("error: ", e))
 
 };
@@ -167,7 +163,7 @@ var fetchNextAndPreviousPages = (path, options={}) => {
 
   return fetch(queryString)
     .then(response => response.json())
-    .then(response => {return response})
+    .then(response => response)
     .catch(error => console.log("error: ", error))
 
 }
